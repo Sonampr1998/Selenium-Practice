d73f38adbc049e05487026c0fcdf26d6f23f163a
Sonampr1998 <praweensonam080@gmail.com>
2024-03-30 00:11:36 +0530

    first commit

---
+++ src/main/java/com/qa/pages/LogInPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class LogInPage {
+	
+	public void loginPage() {
+		System.out.println("login page");
+	}
+
+}
---
+++ src/test/java/com/qa/test/LogInTest.java
@@ -0,0 +1,11 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LogInTest {
+	@Test
+	public void logInTest() {
+		System.out.println("test 1");
+	}
+
+}
